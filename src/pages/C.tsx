aded60a65d80fe5faa280a5fdf4a0c8915f7308b
Jose Collas <joseherminiocollas@goatstone.com>
2022-10-05 06:15:03 -0400

    WIP

--- src/pages/C.tsx
+++ src/pages/C.tsx
@@ -2,18 +2,18 @@ import * as React from "react"
 import B from './B'
 
 const circleCount = 8
-const diskRadius = 3
-const groupRadius = 115
+const diskRadius = 16
+const groupRadius = 120
 const inc = 23
 const circleStart = 190
-const offsetX = 145
-const offsetY = 140
+const offsetX = 143
+const offsetY = 135
 
 const C = () => {
   const RADIANS = Math.PI/180;
   const {cos, sin, sqrt} = Math;
   
-  return (<svg x="10" y="20" width="90%" height="90%" >
+  return (<svg x="0" y="0" width="100%" height="100%" >
     {[...Array(circleCount)].map((v, i) => {
       const circlePlacement = circleStart + i * inc
       const x = Math.cos(circlePlacement * (Math.PI / 180)) * groupRadius + offsetX
@@ -22,7 +22,7 @@ const C = () => {
       return (
         <>
           <B x={x} y={y} radius={diskRadius} />
-          <circle fill="blue" cx="50%" cy="50%" r="22" opacity="0.2" id="c" />
+          <circle fill="blue" cx="50%" cy="70%" r="40" opacity="1" id="c" />
         </>
       )
     })}
--- src/pages/Container.tsx
+++ src/pages/Container.tsx
@@ -4,6 +4,7 @@ import C from './C'
 
 const container = {
     // margin: '50px',
+    position: 'relative'
 }
 const c = {
     fill: 'red',
@@ -34,21 +35,29 @@ const Container = () => {
         <div style={container}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200">
                 <rect style={bg} />
-
                 {/* wedge */}
                 <path d="M 80 80
                     A 45 45, 0, 0, 0, 125 125
                     L 125 80   Z"
                     style={q}
                 />
+                <path d="M 8  8 
+                    A 45 45, 0, 0, 0, 25 25
+                    L 25 8   Z"
+                    style={q}
+                />
                 {/* <circle cx="80" cy="80" r="2" fill="yellow" />
                 <circle cx="125" cy="80" r="2" fill="red" />
                 <circle cx="125" cy="125" r="2" fill="blue" /> */}
                 {/* arc */}
-                <path d="M  50 150
-                    A 60 60 0 0 1 250 150
-                    L 250 150"
+                <path  id="abc" d="M  50 150
+                    A 60 60 0 0 1 250 150"
                     style={r} />
+                <text>
+                    <textPath alignment-baseline="top" xlinkHref="#abc" startOffset="23%">
+                      Dangerous Curves Ahead
+                    </textPath>
+                </text>
                 {/* <circle cx="150.71" cy="170.29" r="2" fill="red" />
                 <circle cx="110" cy="215" r="2" fill="red" />
                 <ellipse cx="144.931" cy="229.512" rx="36" ry="60" fill="transparent" stroke="blue" />
@@ -72,6 +81,20 @@ const Container = () => {
                 {/* <path id="arc" d="M50 10 a 20 20 60 1 0 50 10" style={p} />
                 <path id="arc" d="M50 50 a 10 10 0 1 0 20 20" style={p} /> */}
             </svg>
+            <div id="d" style={
+                {
+                    position: 'absolute',
+                    transformOrigin: 'center center',
+                    left: '50%',
+                    top: '33%',
+                    borderRadius: '50%',
+                    border: '2px solid black',
+                    width: 30,
+                    height: '10%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>xxx sss ccc</div>
         </div>
     )
 }
