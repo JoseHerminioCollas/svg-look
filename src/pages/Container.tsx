38dfbab1128bdd9a02ccc6dbe84f9a461f86f22e
Jose Collas <joseherminiocollas@goatstone.com>
2022-10-10 07:19:07 -0400

    WIP

--- src/pages/Container.tsx
+++ src/pages/Container.tsx
@@ -66,26 +66,11 @@ const Container = () => {
                         </textPath>
                     </text>
                     <C />
-                    {/* CircleGroup */}
-                    {/* Arc */}
-                    {/* Wedge */}
-                    {/* <path d={`M 0 0 
-                    L 90 0 
-                    A 100 100, 0, 0, 0, ${x} ${y} Z`}
-                        style={{ ...wedge, fill: 'green' }}
-                    />
-                    <path d={`M 0 0 
-                    L 90 0 
-                    A 100 100, 0, 0, 0, ${x} ${y} Z`}
-                        style={{ ...wedge, transform: 'rotate(-60deg)' }}
-                    />
-                    <path d={`M 0 0 
-                    L 90 0 
-                    A 100 100, 0, 0, 0, ${x} ${y} Z`}
-                        style={{ ...wedge, transform: 'rotate(-120deg)' }}
-                    /> */}
-                    {/* place text! */}
-                    {['A', 'B', 'C'].map((label, i) => {
+                    {[
+                        { label: 'A', fill: 'green' },
+                        { label: 'B', fill: 'blue' },
+                        { label: 'C', fill: 'red' }
+                    ].map((e, i) => {
                         const rad = 210 + i * 60
                         const cSize = 60
                         const x = cos(rad * RD) * cSize
@@ -96,24 +81,21 @@ const Container = () => {
                         const cSize2 = 90
                         const x2 = cos(rad2 * RD) * cSize2
                         const y2 = sin(rad2 * RD) * cSize2
-                        
+
                         return (
                             <>
                                 <path d={`M 0 0 
                                     L 90 0 
                                     A 100 100, 0, 0, 0, ${x2} ${y2} Z`}
                                     style={{
-                                        ...wedge, fill: 'green',
+                                        ...wedge, fill: e.fill,
                                         transform: `rotate(${240 + i * 60}deg)`
                                     }}
                                 />
-                                <circle cx={x} cy={y} r="10"
-                                    fill="purple"
-                                ></circle>
                                 <text
                                     dominant-baseline="middle" text-anchor="middle"
                                     x={x} y={y} style={{ fill: 'black' }}>
-                                    {label}
+                                    {e.label}
                                 </text>
                             </>)
                     })}
