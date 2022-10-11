8e2d316325b30051b9187b031dfabad75682db08
Jose Collas <joseherminiocollas@goatstone.com>
2022-10-11 09:16:14 -0400

    WIP

--- src/pages/W.tsx
+++ src/pages/W.tsx
@@ -15,7 +15,14 @@ const W = () => {
   const y2 = sin(rad2 * RD) * cSize2
 // wedge corner start = 240
  // wedgeIncrement = 60
-  return (
+ const a: any = []
+ const [s, setS] = React.useState(1)
+ const [s1, setS1] = React.useState(1)
+ const [s2, setS2] = React.useState(1)
+// const states = [s, s1, s2]
+const states2: any = [[s, setS], [s1, setS1], [s2, setS2]]
+// states.push(s)
+ return (
     <>
       {[
         { label: 'A', fill: 'green' },
@@ -30,19 +37,33 @@ const W = () => {
         const tX = cos(textDeg * RD) * textGroupRadius
         const tY = sin(textDeg * RD) * textGroupRadius
 
+        a.push(<circle cx={12}  cy={12} r={100} fill="blue" />)
         return (
           <>
+            {/* {a} */}
             <path d={`M 0 0 
               L 90 0 
               A 100 100, 0, 0, 0, ${x2} ${y2} Z`}
               style={{
                 ...wedge, fill: e.fill,
-                transform: `rotate(${240 + i * 60}deg)`
+                transform: `rotate(${240 + i * 60}deg) scale(${states2[i][0]})`
               }}
+              data-id = {i}
               onClick={(e: any) => {
+                // setS1(1)
+                // setS(2)
+                // states[0](1)
                 // erases previous transform !!!
-                e.target.style.transform = 'scale(1.2)'
-                console.log('t', this, e.target)
+                // document.querySelector('path').style.transform = 'scale(0.2)' 
+                // e.target.style.transform = 'scale(1.2)' + `rotate(${240 + i * 60}deg)`
+                // a.forEach((e: any)  => {
+                //   e.fill = 'red' 
+                // });
+                states2[0][1](1)
+                states2[1][1](1)
+                states2[2][1](1)
+                states2[e.target.dataset.id][1](1.4)
+                console.log('t',  e.target.dataset.id )
               }}
             />
             <text
