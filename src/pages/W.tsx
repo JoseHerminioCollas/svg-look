829dcff467bdc212d78dd6e42255d77f5223e0a6
Jose Collas <joseherminiocollas@goatstone.com>
2022-10-11 09:51:44 -0400

    WIP

--- src/pages/W.tsx
+++ src/pages/W.tsx
@@ -2,27 +2,25 @@ import * as React from "react"
 
 const RD = Math.PI / 180;
 const { cos, sin } = Math;
-const wedge = {
-  fill: "blue", stroke: "black", strokeWidth: "2",
-  transform: 'rotate(-0deg)'
-}
+
+// wedge corner
+const wedgeCornerDeg = -60
+const wedgeRadius = 90
+const wX = cos(wedgeCornerDeg * RD) * wedgeRadius
+const wY = sin(wedgeCornerDeg * RD) * wedgeRadius
 
 const W = () => {
-  // wedge corner
-  const rad2 = -60
-  const cSize2 = 90
-  const x2 = cos(rad2 * RD) * cSize2
-  const y2 = sin(rad2 * RD) * cSize2
-// wedge corner start = 240
- // wedgeIncrement = 60
- const a: any = []
- const [s, setS] = React.useState(1)
- const [s1, setS1] = React.useState(1)
- const [s2, setS2] = React.useState(1)
-// const states = [s, s1, s2]
-const states2: any = [[s, setS], [s1, setS1], [s2, setS2]]
-// states.push(s)
- return (
+  const [s, setS] = React.useState(1)
+  const [s1, setS1] = React.useState(1)
+  const [s2, setS2] = React.useState(1)
+  const states2: any = [[s, setS], [s1, setS1], [s2, setS2]]
+
+  const action = (id: any) => {
+    states2.forEach((e: any) => (e[1](1)))
+    states2[id][1](1.1)
+  }
+
+  return (
     <>
       {[
         { label: 'A', fill: 'green' },
@@ -37,33 +35,18 @@ const states2: any = [[s, setS], [s1, setS1], [s2, setS2]]
         const tX = cos(textDeg * RD) * textGroupRadius
         const tY = sin(textDeg * RD) * textGroupRadius
 
-        a.push(<circle cx={12}  cy={12} r={100} fill="blue" />)
         return (
           <>
-            {/* {a} */}
             <path d={`M 0 0 
               L 90 0 
-              A 100 100, 0, 0, 0, ${x2} ${y2} Z`}
+              A 100 100, 0, 0, 0, ${wX} ${wY} Z`}
               style={{
-                ...wedge, fill: e.fill,
+                fill: e.fill,
                 transform: `rotate(${240 + i * 60}deg) scale(${states2[i][0]})`
               }}
-              data-id = {i}
-              onClick={(e: any) => {
-                // setS1(1)
-                // setS(2)
-                // states[0](1)
-                // erases previous transform !!!
-                // document.querySelector('path').style.transform = 'scale(0.2)' 
-                // e.target.style.transform = 'scale(1.2)' + `rotate(${240 + i * 60}deg)`
-                // a.forEach((e: any)  => {
-                //   e.fill = 'red' 
-                // });
-                states2[0][1](1)
-                states2[1][1](1)
-                states2[2][1](1)
-                states2[e.target.dataset.id][1](1.4)
-                console.log('t',  e.target.dataset.id )
+              data-id={i}
+              onClick={({ target }: any) => {
+                action(target.dataset.id)
               }}
             />
             <text
